f927e7f0c975b4e2dad9966a2b626ca5e068b3d5
042A <johanahlstrand@Johans-MacBook-Pro.local>
2023-06-08 12:20:35 +0200

    inital commit

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "hello world"
+
+    #r = requests.get('https://a.4cdn.org/pol/catalog.json')
+    #r = r.json() 
+    # print ("Returning 4Chan Raw Cat Data")
+    #return r
